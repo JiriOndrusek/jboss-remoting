05ac6e162db725139d46683e28f2743be9a08629
Jean-Francois Denise <jfdenise@gmail.com>
2017-07-05 11:10:11 +0200

    Fix for REM3-293. Avoid resume/suspend read race on server

--- src/main/java/org/jboss/remoting3/remote/ServerConnectionOpenListener.java
+++ src/main/java/org/jboss/remoting3/remote/ServerConnectionOpenListener.java
@@ -137,6 +137,24 @@ final class ServerConnectionOpenListener  implements ChannelListener<ConnectedMe
         }
     }
 
+    private void resumeReads() {
+        connection.getChannel().getIoThread().execute(new Runnable() {
+            @Override
+            public void run() {
+                connection.getChannel().resumeReads();
+            }
+        });
+    }
+
+    private void suspendReads() {
+        connection.getChannel().getIoThread().execute(new Runnable() {
+            @Override
+            public void run() {
+                connection.getChannel().suspendReads();
+            }
+        });
+    }
+
     final class Initial implements ChannelListener<ConnectedMessageChannel> {
         private boolean starttls;
         private Map<String, ?> propertyMap;
@@ -347,7 +365,7 @@ final class ServerConnectionOpenListener  implements ChannelListener<ConnectedMe
                             // bail out
                             return;
                         }
-                        connection.getChannel().suspendReads();
+                        suspendReads();
                         connection.getExecutor().execute(new AuthStepRunnable(true, saslServer, callbackHandler, pooledBuffer, remoteEndpointName, behavior, channelsIn, channelsOut));
                         free = false;
                         return;
@@ -541,7 +559,7 @@ final class ServerConnectionOpenListener  implements ChannelListener<ConnectedMe
                     sendBuffer.flip();
                     connection.send(pooled, close);
                     ok = true;
-                    connection.getChannel().resumeReads();
+                    resumeReads();
                     return;
                 } finally {
                     if (!ok) {
@@ -552,7 +570,6 @@ final class ServerConnectionOpenListener  implements ChannelListener<ConnectedMe
                 buffer.free();
             }
         }
-
 
         private Collection<Principal> createPrincipals() {
             final Set<Principal> principals = new LinkedHashSet<Principal>();
@@ -637,7 +654,7 @@ final class ServerConnectionOpenListener  implements ChannelListener<ConnectedMe
                     }
                     case Protocol.AUTH_RESPONSE: {
                         server.tracef("Server received authentication response");
-                        connection.getChannel().suspendReads();
+                        suspendReads();
                         connection.getExecutor().execute(new AuthStepRunnable(false, saslServer, authorizingCallbackHandler, pooledBuffer, remoteEndpointName, behavior, maxInboundChannels, maxOutboundChannels));
                         free = false;
                         return;
